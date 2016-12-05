58aa32aa76d6a722394649e9ae353ee372542c02
Nick <Nick@DESKTOP-4P1QCAJ>
2016-12-05 13:18:27 -0800

    Add project files.

---
+++ pa8/functions.c
@@ -0,0 +1,13 @@
+#include "functions.h"
+
+int binary_search(int list_values[], int target)
+{
+
+	return 0;
+}
+
+int is_palendrome(char str[], int index_L, int index_R)
+{
+	
+	return 0;
+}
---
+++ pa8/functions.h
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+
+
+char *str_n_copy(char *destination, char *source);
+
+int binary_search(int list_values[], int target);
+
+void bubble_sort();
+
+int is_palendrome(char str[], int index_L, int index_R);
